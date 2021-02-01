f6a659841f416eec4e11bd059bd1015998325a4e
yidan <ys303@duke.edu>
2021-02-01 23:15:49 +0800

    task1

---
+++ src/main/java/edu/duke/ys303/battleship/Board.java
@@ -0,0 +1,22 @@
+package edu.duke.ys303.battleship;
+
+/**
+ * Interface of basic Board
+ */
+
+public interface Board {
+
+  /**
+   * General getWidth method of Board.
+   *
+   * @return width of the board.
+   */
+  public int getWidth();
+
+  /**
+   * General getWidth method of Board.
+   * 
+   * @return width of the board.
+   */
+  public int getHeight();
+}
