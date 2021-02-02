3f73299e2de78fa0a7ebb188bdd5a639db74f7e5
yidan <ys303@duke.edu>
2021-02-02 10:27:17 +0800

    task 3

---
+++ src/main/java/edu/duke/ys303/battleship/BoardTextView.java
@@ -0,0 +1,63 @@
+package edu.duke.ys303.battleship;
+
+/**
+ * This class handles textual display of a Board (i.e., converting it to a
+ * string to show to the user). It supports two ways to display the Board: one
+ * for the player's own board, and one for the enemy's board.
+ */
+public class BoardTextView {
+
+  /**
+   * The Board to display
+   */
+  private final Board toDisplay;
+
+  /**
+   * Constructs a BoardView, given the board it will display.
+   * 
+   * @param toDisplay is the Board to display
+   * @throws IllegalArgumentException if the board is larger than 10x26.
+   */
+  public BoardTextView(Board toDisplay) {
+    this.toDisplay = toDisplay;
+    if (toDisplay.getWidth() > 10 || toDisplay.getHeight() > 26) {
+      throw new IllegalArgumentException(
+          "Board must be no larger than 10x26, but is " + toDisplay.getWidth() + "x" + toDisplay.getHeight());
+    }
+  }
+
+  /**
+   * This makes the header line, e.g. 0|1|2|3|4\n
+   * 
+   * @return the String that is the header line for the given board
+   */
+  String makeHeader() {
+    StringBuilder ans = new StringBuilder("  "); // README shows two spaces at
+    String sep = ""; // start with nothing to separate, then switch to | to separate
+    for (int i = 0; i < toDisplay.getWidth(); i++) {
+      ans.append(sep);
+      ans.append(i);
+      sep = "|";
+    }
+    ans.append("\n");
+    return ans.toString();
+  }
+
+  /**
+   * This display the Text Board.
+   *
+   * @return String of the Board text.
+   */
+  public String displayMyOwnBoard() {
+    String display = this.makeHeader();
+    for (int i = 0; i < toDisplay.getHeight(); i++) {
+      display += (Character.toString('A' + i) + " ");
+      for (int j = 1; j < toDisplay.getWidth(); j++) {
+        display += " |";
+      }
+      display += ("  " + Character.toString('A' + i) + "\n");
+    }
+    display += this.makeHeader();
+    return display;
+  }
+}
