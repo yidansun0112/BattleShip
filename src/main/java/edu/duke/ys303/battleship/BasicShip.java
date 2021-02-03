8e76d22a10e739c810aba06488d2acdca1025d30
yidan <ys303@duke.edu>
2021-02-03 09:44:32 +0800

    task 6

---
+++ src/main/java/edu/duke/ys303/battleship/BasicShip.java
@@ -0,0 +1,40 @@
+package edu.duke.ys303.battleship;
+
+public class BasicShip implements Ship<Character> {
+  private final Coordinate myLocation;
+
+  public BasicShip(Coordinate c) {
+    myLocation = c;
+  }
+
+  @Override
+  public boolean occupiesCoordinates(Coordinate where) {
+    // TODO Auto-generated method stub
+    return where.equals(myLocation);
+  }
+
+  @Override
+  public boolean isSunk() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void recordHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean wasHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Character getDisplayInfoAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return 's';
+  }
+
+}
