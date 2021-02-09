9aae321387f5bef457f6f8206de9a3f427e43b51
yidan <ys303@duke.edu>
2021-02-09 12:51:07 +0800

    task 17

--- src/main/java/edu/duke/ys303/battleship/TextPlayer.java
+++ src/main/java/edu/duke/ys303/battleship/TextPlayer.java
@@ -3,6 +3,10 @@ package edu.duke.ys303.battleship;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.function.Function;
 
 public class TextPlayer {
   final Board<Character> theBoard;
@@ -11,6 +15,8 @@ public class TextPlayer {
   final PrintStream out;
   final AbstractShipFactory<Character> shipFactory;
   final String name;
+  final ArrayList<String> shipsToPlaces;
+  final HashMap<String, Function<Placement, Ship<Character>>> shipCreationFns;
 
   /**
    * Constructor for App
@@ -29,6 +35,30 @@ public class TextPlayer {
     this.inputReader = inputReader;
     this.out = out;
     this.shipFactory = factory;
+    shipsToPlaces = new ArrayList<String>();
+    shipCreationFns = new HashMap<String, Function<Placement, Ship<Character>>>();
+    setupShipCreationMap();
+    setupShipCreationList();
+  }
+
+  /**
+   * Set up shipCreationMap. Put ship creation functions into a map.
+   */
+  protected void setupShipCreationMap() {
+    shipCreationFns.put("Submarine", (p) -> shipFactory.makeSubmarine(p));
+    shipCreationFns.put("Battleship", (p) -> shipFactory.makeBattleship(p));
+    shipCreationFns.put("Destroyer", (p) -> shipFactory.makeDestroyer(p));
+    shipCreationFns.put("Carrier", (p) -> shipFactory.makeCarrier(p));
+  }
+
+  /**
+   * Set up ships list.
+   */
+  protected void setupShipCreationList() {
+    shipsToPlaces.addAll(Collections.nCopies(2, "Submarine"));
+    shipsToPlaces.addAll(Collections.nCopies(3, "Destroyer"));
+    shipsToPlaces.addAll(Collections.nCopies(3, "Battleship"));
+    shipsToPlaces.addAll(Collections.nCopies(2, "Carrier"));
   }
 
   /**
@@ -47,24 +77,25 @@ public class TextPlayer {
   }
 
   /**
-   * Readin a String, construct a placement and place it on the board.
+   * Take a string name, take a proper method and then put the ship on board.
    *
+   * @param String              name of the ship.
+   * @param Function<Placement, Ship<Character>> function to make ship.
    * @throws IOException when Input String is in wrong format. Input String should
    *                     of length three. The first character is an UpperLetter,
    *                     second be a number, and third be an UpperLetter.
    */
-  public void doOnePlacement() throws IOException {
-    Placement p = readPlacement("Player " + name + " Where do you want to place a Destroyer?");
-    Ship<Character> s = shipFactory.makeDestroyer(p);
+  public void doOnePlacement(String shipName, Function<Placement, Ship<Character>> createFn) throws IOException {
+    Placement p = readPlacement("Player " + name + " where do you want to place a " + shipName + "?");
+    Ship<Character> s = createFn.apply(p);
     theBoard.tryAddShip(s);
-    BoardTextView view = new BoardTextView(theBoard);
-    out.println(view.displayMyOwnBoard());
+    out.print(view.displayMyOwnBoard());
   }
 
   /**
-   *Print out prompt and let player to put ship on the board.
+   * Print out prompt and let player to put ships on the board.
    *
-   *@throws IOException when placement is not valid.
+   * @throws IOException when placement is not valid.
    */
   public void doPlacementPhase() throws IOException {
     BoardTextView view = new BoardTextView(theBoard);
@@ -75,6 +106,8 @@ public class TextPlayer {
     out.println("3 \"Destroyers\" ships that are 1x3");
     out.println("3 \"Battleships\" ships that are 1x4");
     out.println("2 \"Carriers\" ships that are 1x6");
-    doOnePlacement();
+    for (String shipName : shipsToPlaces) {
+      doOnePlacement(shipName, shipCreationFns.get(shipName));
+    }
   }
 }
